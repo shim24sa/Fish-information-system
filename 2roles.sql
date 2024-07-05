a297a6ce0521f03a415f642ca5b192a3c0f08382
Mahyasme <m.ghasemizadehkarkan@student.fdu.edu>
2024-07-05 09:07:10 +0330

    Updated GCD.sql

--- 2roles.sql
+++ 2roles.sql
@@ -1,12 +1,9 @@
 CREATE USER IF NOT EXISTS 'mahya'@'%' IDENTIFIED BY '201828Bar';
 CREATE USER IF NOT EXISTS 'shima'@'%' IDENTIFIED BY 'Shima2024';
 CREATE USER IF NOT EXISTS 'app'@'%' IDENTIFIED BY 'CT4WzxeLhHOwxTdoU1TThQzMpD3TwWp4';
-CREATE ROLE IF NOT EXISTS 'Auth_developer','fish_admin','app';
-GRANT ALL PRIVILEGES ON fish_db.* TO  'app' ;
-GRANT ALL PRIVILEGES ON fish_db.* TO 'Auth_developer';
-REVOKE ALL PRIVILEGES ON  fish_db.fishes_table TO 'Auth_developer';
+CREATE ROLE IF NOT EXISTS 'Auth_developer','fish_admin','app_wr';
+GRANT ALL PRIVILEGES ON fish_db.* TO  'app_wr' ;
 GRANT ALL PRIVILEGES ON  fish_db.fishes_table TO 'fish_admin';
-GRANT 'Auth_developer' to 'shima'@'%';
 GRANT 'fish_admin' to  'mahya'@'%';
-GRANT 'app' to 'app'@'%';
+GRANT 'app_wr' to 'app'@'%';
 FLUSH PRIVILEGES;
